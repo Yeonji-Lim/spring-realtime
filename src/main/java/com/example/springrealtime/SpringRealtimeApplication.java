087d3d4e9944da7ba53761e826824970e769bd0c
Yeonji-Lim <view7186@naver.com>
2023-05-26 14:43:05 +0900

    project init

---
+++ src/main/java/com/example/springrealtime/SpringRealtimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRealtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRealtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrealtime/SpringRealtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrealtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRealtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
